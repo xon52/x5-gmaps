62e3756e286bc87a67328eefd42640656f424409
xon52 <30713944+xon52@users.noreply.github.com>
2020-04-20 11:51:35 +1000

    Added anchors to example titles

--- example/ExampleWrapper.vue
+++ example/ExampleWrapper.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="wrapper">
-    <h2 v-if="title" class="title">{{ title }}</h2>
+  <div class="wrapper" :id="anchor">
+    <h2 v-if="title" class="title">{{ title }} <a :href="`#${anchor}`">#</a></h2>
     <div class="description">
       <slot name="description"></slot>
     </div>
@@ -24,6 +24,11 @@ export default {
   name: 'ExampleWrapper',
   props: {
     title: { type: String, default: null }
+  },
+  computed: {
+    anchor() {
+      return this.title.toLowerCase().replace(/\s/g, '')
+    }
   }
 }
 </script>
