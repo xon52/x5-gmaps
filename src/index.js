1b64a36d9601e9196da55d5fbd3e2237fbc2c319
xon52 <30713944+xon52@users.noreply.github.com>
2020-04-21 15:56:24 +1000

    Replaced plugin default install with named method

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ export { default as gmapsHeatmap } from './components/Heatmap.vue'
 export { default as gmapsPopup } from './components/Popup.vue'
 import './scss/index.scss'
 
-export default function(Vue, options) {
+const install = (Vue, options) => {
   if (!options) throw new Error(`x5-gmaps :: (Google API) 'key' is required for plugin install.`)
   const key = options.key || options
   const opt = { ...options }
   init(key, opt)
   Vue.prototype.$GMaps = () => getMap()
   if (process.env.NODE_ENV === 'development')
-    console.warn('x5-gmaps note: marker @positionChanged event has been depreciated; please use @move instead.')
+    console.warn('x5-gmaps note: marker @positionChanged event has been depreciated; please use @move instead.') // eslint-disable-line no-console
 }
+
+export default install
