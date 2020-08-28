6e4ea5bcc152aba098d17ce147e72f6d5278c07b
xon52 <30713944+xon52@users.noreply.github.com>
2020-08-28 14:24:33 +1000

    Added changedPath event to Polygon and Polyline

--- src/components/Polygon.vue
+++ src/components/Polygon.vue
@@ -19,7 +19,7 @@ export default {
   },
   data: () => ({ polygon: null }),
   computed: {
-    _options: vm => ({
+    _options: (vm) => ({
       clickable: vm.clickable,
       draggable: vm.draggable,
       editable: vm.editable,
@@ -35,22 +35,36 @@ export default {
       zIndex: vm.zIndex
     })
   },
+  methods: {
+    changedPath() {
+      if (this.polygon.getEditable()) {
+        const result = this.polygon
+          .getPath()
+          .getArray()
+          .map((e) => e.toJSON())
+        this.polygon.setPath(result)
+        this.$emit('pathChanged', result)
+      }
+    }
+  },
   mounted() {
     this.$GMaps()
-      .then(GMaps => {
+      .then((GMaps) => {
         this.polygon = new GMaps.Polygon({
           map: this.getMap(),
           options: { ...this._options }
         })
       })
-      .then(() => this.polygon.addListener('click', e => this.$emit('click', e)))
-      .then(() => this.polygon.addListener('dblclick', e => this.$emit('doubleClick', e)))
-      .then(() => this.polygon.addListener('drag', e => this.$emit('drag', e)))
-      .then(() => this.polygon.addListener('dragend', e => this.$emit('dragEnd', e)))
-      .then(() => this.polygon.addListener('dragstart', e => this.$emit('dragStart', e)))
-      .then(() => this.polygon.addListener('mouseover', e => this.$emit('mouseover', e)))
-      .then(() => this.polygon.addListener('rightclick', e => this.$emit('rightClick', e)))
-      .catch(e => this.handleError(e))
+      .then(() => this.polygon.addListener('click', (e) => this.$emit('click', e)))
+      .then(() => this.polygon.addListener('dblclick', (e) => this.$emit('doubleClick', e)))
+      .then(() => this.polygon.addListener('drag', (e) => this.$emit('drag', e)))
+      .then(() => this.polygon.addListener('dragend', (e) => this.$emit('dragEnd', e)))
+      .then(() => this.polygon.addListener('dragstart', (e) => this.$emit('dragStart', e)))
+      .then(() => this.polygon.addListener('rightclick', (e) => this.$emit('rightClick', e)))
+      // NOTE: path events insert_at and set_at only fired once so mouse up was more reliable (but mouse up doesn't trigger for remove event)
+      .then(() => this.polygon.addListener('mouseup', () => this.changedPath()))
+      .then(() => this.polygon.getPath().addListener('remove_at', () => this.changedPath()))
+      .catch((e) => this.handleError(e))
   },
   watch: {
     _options(newVal) {
--- src/components/Polyline.vue
+++ src/components/Polyline.vue
@@ -17,7 +17,7 @@ export default {
   },
   data: () => ({ polyline: null }),
   computed: {
-    _options: vm => ({
+    _options: (vm) => ({
       clickable: vm.clickable,
       draggable: vm.draggable,
       editable: vm.editable,
@@ -31,22 +31,36 @@ export default {
       zIndex: vm.zIndex
     })
   },
+  methods: {
+    changedPath() {
+      if (this.polyline.getEditable()) {
+        const result = this.polyline
+          .getPath()
+          .getArray()
+          .map((e) => e.toJSON())
+        this.$emit('pathChanged', result)
+      }
+    }
+  },
   mounted() {
     this.$GMaps()
-      .then(GMaps => {
+      .then((GMaps) => {
         this.polyline = new GMaps.Polyline({
           map: this.getMap(),
           options: { ...this._options }
         })
       })
-      .then(() => this.polyline.addListener('click', e => this.$emit('click', e)))
-      .then(() => this.polyline.addListener('dblclick', e => this.$emit('doubleClick', e)))
-      .then(() => this.polyline.addListener('drag', e => this.$emit('drag', e)))
-      .then(() => this.polyline.addListener('dragend', e => this.$emit('dragEnd', e)))
-      .then(() => this.polyline.addListener('dragstart', e => this.$emit('dragStart', e)))
-      .then(() => this.polyline.addListener('mouseover', e => this.$emit('mouseover', e)))
-      .then(() => this.polyline.addListener('rightclick', e => this.$emit('rightClick', e)))
-      .catch(e => this.handleError(e))
+      .then(() => this.polyline.addListener('click', (e) => this.$emit('click', e)))
+      .then(() => this.polyline.addListener('dblclick', (e) => this.$emit('doubleClick', e)))
+      .then(() => this.polyline.addListener('drag', (e) => this.$emit('drag', e)))
+      .then(() => this.polyline.addListener('dragend', (e) => this.$emit('dragEnd', e)))
+      .then(() => this.polyline.addListener('dragstart', (e) => this.$emit('dragStart', e)))
+      .then(() => this.polyline.addListener('mouseover', (e) => this.$emit('mouseover', e)))
+      .then(() => this.polyline.addListener('rightclick', (e) => this.$emit('rightClick', e)))
+      // NOTE: path events insert_at and set_at only fired once so mouse up was more reliable (but mouse up doesn't trigger for remove event)
+      .then(() => this.polyline.addListener('mouseup', () => this.changedPath()))
+      .then(() => this.polyline.getPath().addListener('remove_at', () => this.changedPath()))
+      .catch((e) => this.handleError(e))
   },
   watch: {
     _options(newVal) {
