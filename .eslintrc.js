e3bb8121fe3c6afaae98b25a74e9eb9ff849fd10
xon52 <30713944+xon52@users.noreply.github.com>
2021-05-23 19:27:56 +1000

    Turn off module boundary ESLint Rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'vue/html-self-closing': 'off',
     'vue/max-attributes-per-line': [
       'error',
